442f141c203d97d8ab20b6c6185411b3a2518e2a
Wang Qian <nickwq@gmail.com>
2016-08-18 00:23:31 +0800

    introduce redux with a simple counter

--- app/index.js
+++ app/index.js
@@ -1,8 +1,28 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Reditor from './reditor.jsx';
+import {createStore} from 'redux';
 
 require('./main.css');
+
+const counter = (state =0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state+1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+const store = createStore(counter);
+
+store.subscribe(() => {
+    document.body.innerHTML = store.getState();
+});
+document.addEventListener('click', () =>{
+    store.dispatch({type:'INCREMENT'});
+});
 
 var app = document.createElement('div');
 app.setAttribute("id", "app");
