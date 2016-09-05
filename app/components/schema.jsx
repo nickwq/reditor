6d93fc18c6cd3d5cb364f2a1fbf36ae6455e0a60
Wang Qian <nickwq@gmail.com>
2016-09-06 00:21:44 +0800

    change schema to stateless component

--- app/components/schema.jsx
+++ app/components/schema.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
-export default class Schema extends React.Component {
-    render(){
-        const schema = this.props.comps.map(
-            (comp, index) => (
-                <li
-                    className={comp.selected ? "selected-component":""}
-                    key={index}
-                    onClick={()=>this.props.onSelect(index)}
-                >{comp.name}</li>
-            )
-        );
-        return (
-            <ul id="schema-body">{schema}</ul>
-        );
-    }
-}
+const Schema = (props) => {
+    const schema = props.comps.map(
+        (comp, index) => (
+            <li
+                className={comp.selected ? "selected-component":""}
+                key={index}
+                onClick={()=>props.onSelect(index)}
+            >{comp.name}</li>
+        )
+    );
+
+    return (
+        <ul id="schema-body">{schema}</ul>
+    );
+};
+
+export default Schema;
